c45105570d0e4c4e5021138ab3ef358b8edef676
Franklyn Zhu <zfranklyn@gmail.com>
2016-09-27 12:42:09 -0400

    started testing wiki

--- test/itsaliave.js
+++ test/itsaliave.js
@@ -3,25 +3,59 @@ var chai = require('chai');
 var spies = require('chai-spies');
 chai.use(spies);
 
+var models = require('../models');
+var Page = models.Page;
+var User = models.User;
 
-describe("simple test", function() {
-  it("tests simple addition", function() {
-    expect(2 + 2).to.equal(4)
+
+describe('Page model', function () {
+
+  var testPage;
+
+  beforeEach(function(){
+    testPage = Page.build({
+                title: "title",
+                content: "content",
+                status: "status",
+                tags: "tag1,tag2,tag3"
+            })
   })
-  it('tests asynchronous code', function(done) {
-    var timeNow = Date.now();
-    var timeLater = setTimeout(function() {
-      expect(Date.now() - timeNow).to.be.closeTo(1000, 50)
-      done()
-    }, 1000)
-  })
-  it('tests the spy functionality of chai', function() {
-    var array = [1, 2, 3];
-    var spy = Array.prototype.forEach;
-    chai.spy.on(array, 'forEach')
-    array.forEach(function(e) {
-      return e + 1;
-    })
-    expect('forEach').to.have.been.called(3);
-  })
-})
+    
+
+  describe('Virtuals', function () {
+    describe('route', function () {
+      it('returns the url_name prepended by "/wiki/"', function(){
+        expect(testPage.route.slice(0,6)).to.equal("/wiki/");
+      });
+    });
+    describe('renderedContent', function () {
+      it('converts the markdown-formatted content into HTML');
+    });
+  });
+
+  describe('Class methods', function () {
+    describe('findByTag', function () {
+      it('gets pages with the search tag');
+      it('does not get pages without the search tag');
+    });
+  });
+
+  describe('Instance methods', function () {
+    describe('findSimilar', function () {
+      it('never gets itself');
+      it('gets other pages with any common tags');
+      it('does not get other pages without any common tags');
+    });
+  });
+
+  describe('Validations', function () {
+    it('errors without title');
+    it('errors without content');
+    it('errors given an invalid status');
+  });
+
+  describe('Hooks', function () {
+    it('it sets urlTitle based on title before validating');
+  });
+
+});
